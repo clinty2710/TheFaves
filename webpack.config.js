aa6e63af2799ad68ce8fc3c5f10f1bde19265dce
clinty2710 <clinty2710@hotmail.com>
2024-06-28 15:28:42 -0400

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -7,104 +7,95 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
+// Load environment variables from .env file
 dotenv.config();
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
-  const plugins = [
+module.exports = {
+  mode: 'production',
+  entry: './frontend/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'frontend', 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(ttf|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'fonts',
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
+    fallback: {
+      fs: false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      querystring: require.resolve('querystring-es3'),
+      zlib: require.resolve('browserify-zlib'),
+      path: require.resolve('path-browserify'),
+      vm: require.resolve('vm-browserify'),
+      timers: require.resolve('timers-browserify'),
+      net: false,
+      os: require.resolve('os-browserify/browser'),
+    }
+  },
+  plugins: [
     new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    }),
     new HtmlWebpackPlugin({
       template: './frontend/public/index.html',
+      inject: 'body',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
+    new CompressionPlugin({
+      test: /\.(js|css|html|ttf|woff2)$/,
+      algorithm: 'gzip',
     }),
-  ];
-
-  if (isProduction && process.env.ANALYZE) {
-    const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-    plugins.push(new BundleAnalyzerPlugin());
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: true,
+    minimizer: [
+      '...', // This syntax extends existing minimizers (i.e. `terser-webpack-plugin`), if any
+      new CssMinimizerPlugin(), // Ensure CSS is also minimized
+    ],
+  },
+  externals: {
+    express: 'express'
   }
-
-  return {
-    mode: isProduction ? 'production' : 'development',
-    entry: './frontend/src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'frontend', 'dist'),
-      filename: '[name].[contenthash].js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader']
-        },
-        {
-          test: /\.(ttf|woff2)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'fonts',
-            }
-          }
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      alias: {
-        react: path.resolve('./node_modules/react'),
-      },
-      fallback: {
-        fs: false,
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        querystring: require.resolve('querystring-es3'),
-        zlib: require.resolve('browserify-zlib'),
-        path: require.resolve('path-browserify'),
-        vm: require.resolve('vm-browserify'),
-        timers: require.resolve('timers-browserify'),
-        net: false,
-        os: require.resolve('os-browserify/browser'),
-      }
-    },
-    plugins,
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin(),
-        new CssMinimizerPlugin(),
-      ],
-      splitChunks: {
-        chunks: 'all',
-      },
-    },
-    devServer: {
-      contentBase: path.join(__dirname, 'frontend', 'dist'),
-      compress: true,
-      port: 9000,
-      historyApiFallback: true,
-    },
-  };
 };
