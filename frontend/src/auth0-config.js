0a937fcfa6bf60abbac477369a3baeb49550c6a5
clinty2710 <clinty2710@hotmail.com>
2024-06-30 18:15:42 -0400

    Delete auth0-config.js

--- frontend/src/auth0-config.js
+++
@@ -1,6 +0,0 @@
-// auth0-config.js (commented out for possible Auth0 implementation)
-
-// const domain = 'dev-e2ak2biooipxd1g7.us.auth0.com';
-// const clientId = 'rnrz5blPhdv8z6MjZ1Cv5rpvvI0jZ2FG';
-
-// export { domain, clientId };
