efa036cbbcc9907b045e7567beb347da71c30899
clinty2710 <clinty2710@hotmail.com>
2024-07-01 23:16:04 -0400

    changed url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://myfavessite.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://thefaves-8616b810d2fc.herokuapp.com/';
 
 export default API_BASE_URL;
