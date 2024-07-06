97d9be4f758f615bd575eecc9a2b0cb8f8c12e38
clinty2710 <clinty2710@hotmail.com>
2024-07-06 02:59:56 -0400

    schema revert

--- models/movie.js
+++ models/movie.js
@@ -1,9 +1,8 @@
 // models/movie.js
 
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const movieSchema = new Schema({
+const movieSchema = new mongoose.Schema({
   _id: {
     type: String,
     required: true,
@@ -13,29 +12,11 @@ const movieSchema = new Schema({
     required: true
   },
   release_date: {
-    type: Date,
-    required: true
+    type: Date
   },
   poster_path: {
     type: String
   }
 });
 
-// Ensuring the index is not unique
-movieSchema.index({ _id: 1 }, { unique: true }); // _id should remain unique
-movieSchema.index({ title: 1, release_date: 1 }); // Example of a composite index
-
-const Movie = mongoose.model('Movie', movieSchema);
-
-// Function to handle index changes
-async function ensureIndexes() {
-  try {
-    await Movie.syncIndexes(); // Sync indexes to match schema
-    console.log('Movie indexes synchronized');
-  } catch (error) {
-    console.error('Error synchronizing movie indexes:', error);
-  }
-}
-
-// Export both the model and the function
-module.exports = { Movie, ensureIndexes };
+module.exports = mongoose.model('Movie', movieSchema);
--- models/music.js
+++ models/music.js
@@ -1,9 +1,8 @@
 // models/music.js
 
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const musicSchema = new Schema({
+const musicSchema = new mongoose.Schema({
   _id: {
     type: String,
     required: true,
@@ -21,27 +20,7 @@ const musicSchema = new Schema({
   artist: {
     type: String,
     required: true
-  },
-  cover_image: {
-    type: String
   }
 });
 
-// Ensuring the index is not unique
-musicSchema.index({ _id: 1 }, { unique: true }); // _id should remain unique
-musicSchema.index({ title: 1, artist: 1 }); // Example of a composite index
-
-const Music = mongoose.model('Music', musicSchema);
-
-// Function to handle index changes
-async function ensureIndexes() {
-  try {
-    await Music.syncIndexes(); // Sync indexes to match schema
-    console.log('Music indexes synchronized');
-  } catch (error) {
-    console.error('Error synchronizing music indexes:', error);
-  }
-}
-
-// Export both the model and the function
-module.exports = { Music, ensureIndexes };
+module.exports = mongoose.model('Music', musicSchema);
