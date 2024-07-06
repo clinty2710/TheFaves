381e451aa8d5a6e09357e9d23c41b6c25b1a4b84
clinty2710 <clinty2710@hotmail.com>
2024-07-06 01:52:20 -0400

    Update Favorites.js

--- routes/Favorites.js
+++ routes/Favorites.js
@@ -105,8 +105,8 @@ router.post('/add', async (req, res) => {
         item_Id: bookId,
         item_Type: item_Type,
         book: {
-          title: bookTitle.split(' by ')[0],
-          author: author,
+          title: title || bookTitle,
+          author: authorName || author,
           cover_image: coverImage
         }
       });
