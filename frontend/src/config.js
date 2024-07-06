91bd7246f09421fcd17eb18566b881b570d76d48
clinty2710 <clinty2710@hotmail.com>
2024-07-06 00:07:34 -0400

    updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,4 @@
 // src/config.js
 
-const API_BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'https://thefaves-8616b810d2fc.herokuapp.com'
-  : 'http://localhost:3000';
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export default API_BASE_URL;
