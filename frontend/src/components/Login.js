087a245ccbf031266288216dfdec8504d1f24b5e
clinty2710 <clinty2710@hotmail.com>
2024-07-19 15:18:46 -0400

    changing size

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -51,6 +51,7 @@ const Login = () => {
           <span className="s">S</span>
         </div>
       </header>
+      <h2>Login</h2>
       <form onSubmit={handleLogin}>
         <div>
           <label htmlFor="email">Email:</label>
