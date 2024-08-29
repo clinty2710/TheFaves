eba5aac5c49f059e87b9eb0c5198217d419914b4
clinty2710 <clinty2710@hotmail.com>
2024-08-29 17:58:46 -0400

    updates

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -95,11 +95,11 @@ const Profile = () => {
       <div className="section-container">
         <div className="search-column">
           <div className="search-container">
+            <h2>Favorite Movies</h2>
             <SearchMovies favorites={favorites} setFavorites={setFavorites} />
           </div>
         </div>
         <div className="favorites-column">
-        <h2>Favorite Movies</h2>
           <div className="favorites-section">
             {Array.isArray(favorites) && favorites.filter(fav => fav.item_Type === 'movie').map(fav => (
               <div key={fav._id} className="favorite-item">
