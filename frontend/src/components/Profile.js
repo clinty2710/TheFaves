e9ad311612d477004da5baa9a5aed7b3585d1b78
clinty2710 <clinty2710@hotmail.com>
2024-09-06 11:16:52 -0400

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -79,8 +79,6 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-      <div className="vertical-line-top"></div> {/* Vertical line added */}
-
       <header className="profile-header">
         <div className="logo">
           <span className="my">my</span>
@@ -90,6 +88,7 @@ const Profile = () => {
           <span className="e">E</span>
           <span className="s">S</span>
         </div>
+        <div className="vertical-line-top"></div> {/* Vertical line added */}
         <span className="welcome-message">Welcome, {user.nickname}!</span>
       </header>
 
